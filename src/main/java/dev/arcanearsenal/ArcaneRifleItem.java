1805dcf4d1857d0aa84a9936affab10c71b4afa6
arroyososa08 <j-arroyososa@outlook.com>
2026-09-23 12:31:39 -0400

    Update ArcaneRifleItem.java

--- src/main/java/dev/arcanearsenal/ArcaneRifleItem.java
+++ src/main/java/dev/arcanearsenal/ArcaneRifleItem.java
@@ -18,13 +18,36 @@ public class ArcaneRifleItem extends Item {
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
 
+        var rifle = player.getItemInHand(hand);
+
         if (level.isClientSide()) {
             return InteractionResult.SUCCESS;
         }
 
+        int loadedAmmo = rifle.getOrDefault(
+                ModComponents.LOADED_AMMO,
+                0
+        );
+
+        if (loadedAmmo <= 0) {
+            ArcaneArsenal.LOGGER.info(
+                    "{} tried to fire an empty Arcane Rifle.",
+                    player.getName().getString()
+            );
+
+            return InteractionResult.FAIL;
+        }
+
+        rifle.set(
+                ModComponents.LOADED_AMMO,
+                loadedAmmo - 1
+        );
+
         ArcaneArsenal.LOGGER.info(
-                "{} fired the Arcane Rifle!",
-                player.getName().getString()
+                "{} fired the Arcane Rifle! Ammo: {}/{}",
+                player.getName().getString(),
+                loadedAmmo - 1,
+                MAGAZINE_SIZE
         );
 
         return InteractionResult.SUCCESS;
