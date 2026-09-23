da3846b9e93c6f2ef48f888985a4b205a20d53bb
arroyososa08 <j-arroyososa@outlook.com>
2026-09-23 11:52:07 -0400

    Add ArcaneRifleAmmoItem class for ammo item

---
+++ src/main/java/dev/arcanearsenal/ArcaneRifleAmmoItem.java
@@ -0,0 +1,10 @@
+package dev.arcanearsenal;
+
+import net.minecraft.world.item.Item;
+
+public class ArcaneRifleAmmoItem extends Item {
+
+    public ArcaneRifleAmmoItem(Properties properties) {
+        super(properties);
+    }
+}
