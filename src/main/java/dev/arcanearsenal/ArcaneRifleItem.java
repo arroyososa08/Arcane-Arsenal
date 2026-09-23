538cba50aae5f21e87d3ef5a6f83beb3b92784c7
arroyososa08 <j-arroyososa@outlook.com>
2026-09-23 11:42:57 -0400

    Add ArcaneRifleItem class to the project

---
+++ src/main/java/dev/arcanearsenal/ArcaneRifleItem.java
@@ -0,0 +1,10 @@
+package dev.arcanearsenal;
+
+import net.minecraft.world.item.Item;
+
+public class ArcaneRifleItem extends Item {
+
+    public ArcaneRifleItem(Properties properties) {
+        super(properties);
+    }
+}
